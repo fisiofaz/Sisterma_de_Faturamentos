68ade17ec4bb6037c5835e0eaa00dc109187c0de
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-05-02 06:25:59 -0300

    refatomaneto de login

--- js/login.js
+++ js/login.js
@@ -1,22 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const form = document.getElementById('login-form');
-    const errorMessage = document.getElementById('error-message');
+    const mensagem = document.getElementById('mensagem-usuario');
+    const usuarioInput = document.getElementById('usuario');
+    const ultimoUsuario = localStorage.getItem('ultimoUsuario');
   
+    if (ultimoUsuario) {
+      mensagem.textContent = `Último login: ${ultimoUsuario}`;
+      usuarioInput.value = ultimoUsuario;
+    }
+  
+    const form = document.getElementById('login-form');
     form.addEventListener('submit', (e) => {
       e.preventDefault();
   
-      const username = form.username.value.trim();
-      const password = form.password.value.trim();
+      const usuario = usuarioInput.value.trim();
+      const senha = document.getElementById('senha').value;
   
-      // Credenciais fixas por enquanto
-      if (username === 'admin' && password === '1234') {
-        localStorage.setItem('usuarioLogado', JSON.stringify({ username }));
-        window.location.href = '../../index.html'; 
+      if (usuario === 'admin' && senha === '1234') {
+        localStorage.setItem('usuarioLogado', usuario);
+        localStorage.setItem('ultimoUsuario', usuario);
+        window.location.href = '../index.html';
       } else {
-        errorMessage.textContent = 'Usuário ou senha inválidos.';
+        alert('Usuário ou senha inválidos');
       }
     });
   });
+  
 
+document.addEventListener('DOMContentLoaded', () => {
+    const mensagem = document.getElementById('mensagem-usuario');
+    const ultimoUsuario = localStorage.getItem('ultimoUsuario');
   
+    if (ultimoUsuario) {
+      mensagem.textContent = `Último login: ${ultimoUsuario}`;
+    }
+});
   
+    
