0ba8b485bc07e24559a2e02b98d0d0fb4ba15432
fisiofaz <75371386+fisiofaz@users.noreply.github.com>
2025-05-09 16:46:56 -0300

    dando inicio ao react no projeto

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './style.css'; // Importe seu arquivo CSS
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sistema de Faturamento</h1>
+        <p>Bem-vindo ao novo frontend com React!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
